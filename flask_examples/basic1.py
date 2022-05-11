ac46b3925786ac0e583254482e67d7eb9e29086d
mcndoungerii <allenmcndounger@gmail.com>
2022-05-11 16:37:14 +0300

    Create var in python, send those variables to the template, and inject them using jinja template literals

--- flask_examples/basic1.py
+++ flask_examples/basic1.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('basic.html')
+    name = "Jose"
+    letters = list(name)
+    pup_dictionary = {'pup_name': 'Sammy','age': 21}
+    return render_template('basic.html',name=name,letters=letters,
+    pup_dictionary=pup_dictionary)
 
 if __name__ == "__main__":
     app.run(debug=True)
