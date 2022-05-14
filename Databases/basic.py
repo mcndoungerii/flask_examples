f265928361638301e59ccb2df79d626af8ddc2e3
mcndoungerii <allenmcndounger@gmail.com>
2022-05-14 11:02:12 +0300

    Flask & Databases

---
+++ Databases/basic.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+#__file__ is the path of the current file (i.e. basic.py)
+#when a module is loaded in python, this file var is built in and that's set to the name of the actual file
+#os.path.abspath(os.path.dirname(__file__)) is the path of the directory that contains the current file (e.g) C:\Users\james\Documents\GitHub\python-flask-tutorial\Databases\basic.py
+print(f"Path is {basedir}")
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+####################################################
+class Puppy(db.Model):
+    # Manual TABLE NAME
+    __tablename__ = 'puppies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    age = db.Column(db.Integer)
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    
+    def __repr__(self):
+        return f"Puppy {self.name} is {self.age} year/s old"
