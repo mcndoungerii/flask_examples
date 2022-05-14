eb6b9ed296b76f065b4acafd61c6ad5f50fcd272
mcndoungerii <allenmcndounger@gmail.com>
2022-05-14 13:15:26 +0300

    Flask Basic Queries

--- Databases/basic.py
+++ Databases/basic.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate # pip install Flask-Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 #__file__ is the path of the current file (i.e. basic.py)
@@ -13,6 +14,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+Migrate(app,db) #Migrate is a class that is used to create migrations
+
 ####################################################
 class Puppy(db.Model):
     # Manual TABLE NAME
@@ -21,10 +24,12 @@ class Puppy(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     age = db.Column(db.Integer)
+    breed = db.Column(db.Text)
 
-    def __init__(self,name,age):
+    def __init__(self,name,age,breed):
         self.name = name
         self.age = age
+        self.breed = breed
     
     def __repr__(self):
         return f"Puppy {self.name} is {self.age} year/s old"
